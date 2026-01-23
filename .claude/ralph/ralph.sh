3362153bed138039d5dae468df1d61172c22ae10
lincyaw <814750204@qq.com>
2026-01-23 23:37:51 +0800

    update script

--- .claude/ralph/ralph.sh
+++ .claude/ralph/ralph.sh
@@ -18,15 +18,14 @@ for ((i=1; i<=MAX_ITER; i++)); do
   echo "Iteration $i / $MAX_ITER"
   echo "========================================"
 
-  result=$(claude --dangerously-skip-permissions -p "@.claude/ralph/plans/prd.json @.claude/ralph/progress.txt \
+  result=$(claude --dangerously-skip-permissions -p "@.claude/ralph/plans/prd.json @.claude/ralph/progress.txt  progress.txt may be long, so use tail, or grep to search it by id, like `P0-BE-005`. \
   1. Find the highest-priority feature to work on and work only on that feature. \
   This should be the one YOU decide has the highest priority - not necessarily the first item. \
-  2. If the prd.json is not clear, you can reference .claude/ralph/docs/spec.md and .claude/ralph/docs/task.md, but be careful the files is long \
+  2. If the prd.json is not clear, you can reference .claude/ralph/docs/spec.md and .claude/ralph/docs/task.md, but be careful the files is long, use grep, tail, head to find relevant information. \
   3. Update the PRD with the work that was done. \
-  4. Append your progress to the progress.txt file. \
-  Use this to leave a note for the next person working in the codebase. \
+  4. Append your progress to the progress.txt file, use this to leave a note for the next person working in the codebase. \
   5. If there are some questions you have about the design, implementation, etc., stop your work and ask me. \
-  6. Call proper subagent for specfic tasks. \
+  6. Call proper subagent and use proper skills for specfic tasks, but do not use Plan agent unless you feel it is must. \
   7. Make a git commit of that feature. \
   ONLY WORK ON A SINGLE FEATURE. \
   8. Check CLAUDE.md to see if there are any special instructions for committing code, e.g., format code, lint, etc. \
